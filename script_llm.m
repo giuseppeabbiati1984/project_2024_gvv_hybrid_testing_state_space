8aff4c8329efa30161af78faa7724e50420d3a56
giuseppe <giuseppe.abbiati@gmail.com>
2024-11-26 17:16:17 +0100

    added some comments and checks to the original simulink model with the algorithm

--- script_llm.m
+++ script_llm.m
@@ -3,7 +3,10 @@ clear
 clc
 
 dt=0.01;
-tmax=10.0;
+tmax=1000.0;
+
+coupling = 'a' ; % working fine
+% coupling = 'v' ; % not working (simulink model unstable)
 
 m1=10;
 k1=100;
@@ -25,14 +28,24 @@ O=zeros(2);
 
 A1=[O,I;-M1\K1,-M1\C1];
 B1=[0;0;M1\[0;1]];
+
+if strcmp(coupling,'v')
+    G1=[0,1,0,0];
+elseif strcmp(coupling,'a') %#ok<*UNRCH>
+    G1=[0,0,0,1];
+end
+
 H1=[0;0;0;1];
-G1=[0,0,0,1];
 B1b=-1;
 G1b=-1;
 
 A2=[O,I;-M2\K2,-M2\C2];
 B2=[0;0;M2\[0;1]];
-G2=[0,0,0,1];
+if strcmp(coupling,'v')
+    G2=[0,1,0,0];
+elseif strcmp(coupling,'a') %#ok<*UNRCH>
+    G2=[0,0,0,1];
+end
 H2=[0;0;0;0];
 B2b=-1;
 G2b=-1;
